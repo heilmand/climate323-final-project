49c3ed524959379b0d5b8cde482a95fef53d5b67
Elise Segal <elisesegal@gmail.com>
2025-03-19 11:22:55 -0400

    commit

---
+++ test.py
@@ -0,0 +1,64 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+# ---
+
+# %% [markdown]
+# ### Helpful resources:
+# * [Guide to Markdown](https://paperhive.org/help/markdown)
+# * [Guide to LaTeX Math symbols](http://tug.ctan.org/info/undergradmath/undergradmath.pdf)
+# * [Python Cheat Sheets](https://ehmatthes.github.io/pcc/cheatsheets/README.html)
+# * [Moving to Python from MATLAB](https://bastibe.de/2013-01-20-a-python-primer-for-matlab-users.html)
+#
+# ### Template for lab report:
+# * **Title:** Name the lab that this report is for
+# * **Collaborators:** Team work on labs is encouraged, but everyone is required to turn in their own lab report. Please list your collaborators in the intro to the report.
+# * **Goal and Introduction:** Add a brief description of the goal and background knowledge for the lab. This can be drawn from the lab description, but should be in your own words.
+# * **Data:** List the datasets used, what they describe and any quality/pre-processing before analysis.
+# * **Approach and Results:** Describe your approach for each question in the lab description and interpretation of the results for that question.
+# * **Conclusions:** Synthesize the conclusions from your results section here. Give overarching conclusions. Tell us what you learned.
+# * **References:** Cite any resources or publications you used.
+# ---
+# # Final Project
+# ### Elise Segal, Daniel Heilmen, Natalie Giovi, Percy Slattery
+#
+# ## Goal and Introduction
+# Add a brief description of the goal and background knowledge for the lab. This can be drawn from the lab description, but should be in your own words.
+#
+# ## Data
+# List the datasets used, what they describe and any quality/pre-processing before analysis.
+#
+# ----
+# ## Approach and Results
+# Describe your approach for each question in the lab description and interpretation of the results for that question.
+# Start with an over-arching paragraph to describe your approach as you see fit.
+
+# %% [markdown]
+# ### Question 1
+# Write a function to read the *.csv files using numpy.genfromtxt. Leverage the example above to ensure success.
+#
+#
+
+# %% [markdown]
+# ### Question 2
+# Description of what you need to do and interpretation of results (if applicable)
+#
+#
+
+# %% [markdown]
+# ### Question 3
+# Description of what you need to do and interpretation of results (if applicable)
+#
+
+# %% [markdown]
+# ## Conclusions
+# Synthesize the conclusions from your results section here. Give overarching conclusions. Tell us what you learned.
+# ## References
+# List any references used
